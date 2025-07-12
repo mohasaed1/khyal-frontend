d57bc19cee9e85d5f476a65ed326186f3f170c86
mohasaed1 <125800065+mohasaed1@users.noreply.github.com>
2025-07-12 23:11:25 +0400

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav style={{
+      background: '#8E44AD',
+      color: '#fff',
+      padding: '1rem',
+      textAlign: 'center',
+      fontSize: '1.5rem'
+    }}>
+      🌟 خيال.ai
+    </nav>
+  )
+}
